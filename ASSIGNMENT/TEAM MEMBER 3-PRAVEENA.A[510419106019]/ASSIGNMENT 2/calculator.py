70381e492f3bee29bb7cbe90f3a3bf2c8c182004
praveena123456789 <115065950+praveena123456789@users.noreply.github.com>
2022-10-10 14:47:13 +0530

    Add files via upload

---
+++ ASSIGNMENT/TEAM MEMBER 3-PRAVEENA.A[510419106019]/ASSIGNMENT 2/calculator.py
@@ -0,0 +1,14 @@
+num_1=int(input('enter your first number:'))
+num_2=int(input('enter your second number:'))
+#addition
+print('()+()='.format(num_1,num_2))
+print(num_1+num_2)
+#subtraction
+print('()-()='.format(num_1,num_2))
+print(num_1-num_2)
+#multiplication
+print('()*()='.format(num_1,num_2))
+print(num_1*num_2)
+#divishion
+print('()/()='.format(num_1,num_2))
+print(num_1/num_2)
